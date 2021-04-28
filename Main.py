09c89fed71649646e66d8c3c8844dec3ef9ccf43
Maxine Antao <66948179+maxicles@users.noreply.github.com>
2021-04-28 19:02:22 +0200

    Added restart button and rest function

--- Main.py
+++ Main.py
@@ -17,6 +17,7 @@ pygame.display.set_caption("Run Timmy, Fight Platformer")
 # Loading some images into the game to store them
 sun_image = pygame.image.load('assets/sun.png')
 background_image = pygame.image.load('assets/sky.png')
+restart_image = pygame.image.load('assets/restart_btn.png')
 
 # defining grid variables
 tile_size = 50  # tile size will be 50x50 on the grid
@@ -30,33 +31,40 @@ game_over = 0
 #         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
 #         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
+class Button(): # creatin the class for the load, restart and save buttons
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self):
+        action = False # whatever the mosue button does
+        # checking to see if the button is being pressed throuh the mouse position
+        position = pygame.mouse.get_pos()
+
+        # checking to see if the mouse button has been pressed
+        if self.rect.collidepoint(position):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: # the index 0 is referred to the left mosue button
+                action = True
+                self.clicked = True
+        if pygame.mouse.get_pressed()[0] == 0: # if you've released the mouse button its not pressed and nothing with happen
+            self.clicked = False
+
+
+
+        # drawing the vutton images on the screen
+        screen.blit(self.image, self.rect)
+
+        return action
+
+
+
 
 class Player():
     def __init__(self, x, y):
-        self.anim_right = []
-        self.anim_left = []
-        self.index = 0
-        self.counter = 0
-        for num in range(1, 5):
-            player_image_right = pygame.image.load(f'assets/guy{num}.png')
-            player_image_right = pygame.transform.scale(player_image_right,
-                                                        (40, 80))  # loading in the player image and scaling in to size
-            player_image_left = pygame.transform.flip(player_image_right, True,
-                                                      False)  # using the right image thats loaded and flipping it to the left
-            self.anim_right.append(player_image_right)
-            self.anim_left.append(player_image_left)
-        self.player_died = pygame.image.load('assets/ghost.png')
-        self.image = self.anim_right[self.index]
-
-        self.rect = self.image.get_rect()
-        # iving the image an coordinate
-        self.rect.x = x
-        self.rect.y = y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.velocity_y = 0
-        self.jump = False
-        self.direction = 0  # usin the variable to determine whether the player is pressing left or riht
+        self.reset(x, y) # calling the reset method, so when the player dies, The character will be respawned at the beginning
 
     def update(self, game_over):
         dx = 0
@@ -66,10 +74,10 @@ class Player():
         if game_over == 0:
             # adding in controls for the player
             key = pygame.key.get_pressed()
-            if key[pygame.K_SPACE] and self.jump == False:
+            if key[pygame.K_SPACE] and self.jump == False and self.in_air == False:
                 self.velocity_y = -15
                 self.jump = True
-            if key[pygame.K_SPACE]:
+            if key[pygame.K_SPACE] == False:
                 self.jump = False
 
             # calculating the new player position. Checking the collision of that new positiom and the adjustin the position when collinding with an object
@@ -100,10 +108,10 @@ class Player():
                 if self.index >= len(self.anim_right):
                     self.index = 0
                 if self.direction == 1:
-                    # updating the animation with the next imae
+                    # updating the animation with the next image
                     self.image = self.anim_right[self.index]
                 if self.direction == -1:
-                    # updating the animation with the next imae
+                    # updating the animation with the next image
                     self.image = self.anim_left[self.index]
 
             # adding in some gravity when jumping
@@ -113,11 +121,12 @@ class Player():
             dy += self.velocity_y
 
             # check for collision
+            self.in_air = True # checkin if the player is in the air, assuming he is..
             for tile in world.tile_list:  # lookin for collison in the world tile list from the y direction
                 # checking for collision in x direction
                 if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                     dx = 0
-
+                    #checking for collison in the y direction
                 if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width,
                                        self.height):  # creatin a new collision detection box which wil act as a temporary collision box, in orde to make adjustmenst before the actual collision
                     # checkin if the player is below the ground. if the player is jumping
@@ -127,6 +136,8 @@ class Player():
                     # checking if the player is above the ground and is fallin
                     elif self.velocity_y >= 0:
                         dy = tile[1].top - self.rect.bottom
+                        self.velocity_y = 0
+                        self.in_air = False
 
             #checking if the character collides with the enemies
             if pygame.sprite.spritecollide(self, blob_group, False):
@@ -153,6 +164,34 @@ class Player():
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
         return game_over
+
+    def reset(self, x, y):
+        self.anim_right = []
+        self.anim_left = []
+        self.index = 0
+        self.counter = 0
+
+        for num in range(1, 5):
+            player_image_right = pygame.image.load(f'assets/guy{num}.png')
+            player_image_right = pygame.transform.scale(player_image_right,
+                                                        (40, 80))  # loading in the player image and scaling in to size
+            player_image_left = pygame.transform.flip(player_image_right, True,
+                                                      False)  # using the right image thats loaded and flipping it to the left
+            self.anim_right.append(player_image_right)
+            self.anim_left.append(player_image_left)
+        self.player_died = pygame.image.load('assets/ghost.png')
+        self.image = self.anim_right[self.index]
+
+        self.rect = self.image.get_rect()
+        # iving the image an coordinate
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.velocity_y = 0
+        self.jump = False
+        self.direction = 0
+        self.in_air = True
 
 
 # creating a class to display all the world data from the lsit below
@@ -254,11 +293,14 @@ world_data = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                1]]  # the first row is going to be the dirt in the 5x5 grid. Defining where all the elements in the game will sit on the grid
 # The bottom row will be for the grass images
 
-
+# instances created to be able to run it in the game
 player = Player(100, screen_height - 130)
 blob_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
 world = World(world_data)
+
+# creatin buttons
+restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_image)
 
 GameIsRunning = True
 while GameIsRunning:
@@ -273,9 +315,21 @@ while GameIsRunning:
     if game_over == 0:
         blob_group.update()
 
+
     blob_group.draw(screen)
     lava_group.draw(screen)
+
     game_over = player.update(game_over)
+
+     # if player has died calling the restart button
+    if game_over == - 1:
+        if restart_button.draw():
+            player.reset(100, screen_height - 130)
+            game_over = 0
+
+
+
+
     # calling the grid
     # draw_grid()
 
